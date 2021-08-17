e987d2ad684e07a1f08c755324f89d3079d903aa
Amit Dhakite <amitdhakite24@gmail.com>
2021-08-17 20:45:21 +0530

    updates

--- multer.js
+++ multer.js
@@ -1,14 +1,18 @@
 const multer = require("multer");
 
-
-
 module.exports = multer({
   storage: multer.diskStorage({}),
-  fileFilter: (req, file, cb)=>{
-    if(!(file.mimetype==='image/jpeg'|| file.mimetype==='image/png'||file.mimetype==='image/jpg')){
-      cb({message:'Unsuported File Format'}, false);
+  fileFilter: (req, file, cb) => {
+    if (
+      !(
+        file.mimetype === "image/jpeg" ||
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg"
+      )
+    ) {
+      cb({ message: "Unsuported File Format" }, false);
       return;
     }
-  cb(null, true)
-}
+    cb(null, true);
+  },
 });
