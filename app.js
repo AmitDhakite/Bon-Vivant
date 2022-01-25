93a1eb2731679ca076a03e7ce5ed287cfcd36040
Amit Dhakite <amitdhakite24@gmail.com>
2022-01-25 12:33:56 +0530

    Connection error fixing

--- app.js
+++ app.js
@@ -68,13 +68,13 @@ var db_config = {
   multipleQueries: true,
 };
 
-var connection;
+var con;
 
 function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
+  con = mysql.createConnection(db_config); // Recreate the connection, since
                                                   // the old one cannot be reused.
 
-  connection.connect(function(err) {              // The server is either down
+  con.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
@@ -83,7 +83,7 @@ function handleDisconnect() {
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
+  con.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
       handleDisconnect();                         // lost due to either server restart, or a
